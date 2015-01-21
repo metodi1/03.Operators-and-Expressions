d77a48db8efb67ab13302e48396e5e3c6fc0f5e7
metodi1 <metodi.ganev@gmail.com>
2015-01-21 18:35:52 +0200

    creation

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,15 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//Write an expression that checks if given positive integer number n (n <= 100) 
+//is prime (i.e. it is divisible without remainder only to itself and 1).
+//Note: You should check if the number is positive
+//Examples:
+//n	Prime?
+//1	false
+//2	true
+//3	true
+//4	false
+//9	false
+//97	true
+//51	false
+//-3	false
+//0	false
 
-namespace ConsoleApplication1
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-        }
     }
 }
+
